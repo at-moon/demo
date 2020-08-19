270f24532b17ad60fe9d1f259513887afb1be853
at-moon <894298646@qq.com>
2020-08-19 17:24:43 +0800

    leetcode

--- src/test/java/com/atmoon/demo/LeetCode.java
+++ src/test/java/com/atmoon/demo/LeetCode.java
@@ -3402,4 +3402,26 @@ public class LeetCode {
         return new TreeNode(root.val, sortedListToBST(head), sortedListToBST(root.next));
     }
 
+    // todo start
+
+    /**
+     * 647. 回文子串
+     *
+     * @param s
+     * @return
+     */
+    public int countSubstrings(String s) {
+        int count = 0;
+        int length = s.length();
+        for (int i = 0; i < length * 2 - 1; i++) {
+            int l = i / 2, r = i / 2 + i % 2;
+            while (l >= 0 && r < length && s.charAt(l) == s.charAt(r)) {
+                l--;
+                r++;
+                count++;
+            }
+        }
+        return count;
+    }
+
 }
