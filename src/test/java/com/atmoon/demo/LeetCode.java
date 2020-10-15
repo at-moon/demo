a4382c969f663bf006bf37d40f9f0515555402cf
at-moon <894298646@qq.com>
2020-10-15 09:52:11 +0800

    leetcode

--- src/test/java/com/atmoon/demo/LeetCode.java
+++ src/test/java/com/atmoon/demo/LeetCode.java
@@ -3094,6 +3094,9 @@ public class LeetCode {
 
     class Node {
         public int val;
+        public Node left;
+        public Node right;
+        public Node next;
         public List<Node> neighbors;
 
         public Node() {
@@ -3104,6 +3107,13 @@ public class LeetCode {
         public Node(int _val) {
             val = _val;
             neighbors = new ArrayList<>();
+        }
+
+        public Node(int _val, Node _left, Node _right, Node _next) {
+            val = _val;
+            left = _left;
+            right = _right;
+            next = _next;
         }
 
         public Node(int _val, ArrayList<Node> _neighbors) {
@@ -4941,6 +4951,39 @@ public class LeetCode {
             }
         }
         return result;
+    }
+
+    /**
+     * 116. 填充每个节点的下一个右侧节点指针
+     *
+     * @param root
+     * @return
+     */
+    public Node connect(Node root) {
+        if (root == null) {
+            return null;
+        }
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+
+        // 逐层遍历
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                Node node = queue.poll();
+                if (i < size - 1) {
+                    node.next = queue.peek();
+                }
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+        }
+        return root;
     }
 
 }
